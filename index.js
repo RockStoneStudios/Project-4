03ef9a8f6e097bb57ba067c1970b39a1c69a0a4c
= <RockStoneStudios666@gmail.com>
2021-01-23 19:10:19 -0500

    Creacion de Carpeta Public y archivo Css

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const config = require('./Config/config');
+const path = require('path');
 
 
 
+// middlewares
 
+app.use(express.static('Public'));
+app.set('view engine','ejs');
+app.set('Views',path.join(__dirname,'Views'));
 
-
+app.get('/',(req,res)=>{
+    res.render('index');
+})
 
 app.listen(config.PORT,()=>{
     console.log('Starting Port....'+config.PORT);
